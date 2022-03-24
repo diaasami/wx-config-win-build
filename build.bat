0d1c9dabf88b4ec0fe205a17327c95283382fe5c
Diaa Sami <diaasami@fastmail.fm>
2022-03-24 14:08:04 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-/mingw32/bin/i686-w64-mingw32-g++ -Os -static-libgcc -static-libstdc++ -o wx-config $GITHUB_WORKSPACE/wx-config-win.cpp
-strip --strip-all $GITHUB_WORKSPACE/wx-config.exe
+/mingw32/bin/i686-w64-mingw32-g++ -Os -static-libgcc -static-libstdc++ -o wx-config $1/wx-config-win.cpp
+strip --strip-all wx-config.exe
