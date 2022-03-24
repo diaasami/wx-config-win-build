42e1b63b19e951763870292529180d60069884df
Diaa Sami <diaasami@fastmail.fm>
2022-03-24 14:33:52 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-/mingw32/bin/i686-w64-mingw32-g++ -Os -static-libgcc -static-libstdc++ -o wx-config $1/wx-config-win.cpp
+/mingw32/bin/i686-w64-mingw32-g++ -Os -static-libgcc -static-libstdc++ -o wx-config %1/wx-config-win.cpp
 strip --strip-all wx-config.exe
